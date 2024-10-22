a67fe97cc3708f6398515faa82921f2786ef5500
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-10-22 10:03:42 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,57 @@
 function flip(array, n) {
-    
-    for(let i =0; i < n; i++){
-        
+    var tmp = [];
+    if (n > array.length){
+        array.reverse();
+        return array;
     }
+    else{
+        for(let i = 0; i < n; i++){
+            tmp.push(array[i]);
+        }
+        tmp.reverse();
+        
+        for(i = 0; i < n; i++){
+            array[i] = tmp[i];
+        }
     return array;
+    }
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    var numSort = 0;
+    while(numSort != array.length){
+        var high = array[0];
+        for (i=0; i< (array.length- numSort0; i++){
+            if (array[i] > high) {
+                high = array[i];
+            }
+        }
+        if (high != array[0]){
+            n = 0;
+            while (n <= array.indexOf(high)){
+                n+= 1;
+                }
+            flip (array, n);
+            if (numSort == 0){
+                flip (array, (array.length + 1));
+                numSort += 1;
+                }
+            else {
+                flip (array, (array.length - numSort));
+                numSort += 1;
+                }
+            }
+        else {
+            if (numSort == 0){
+                flip (array, (array.length + 1));
+                numSort += 1;
+            }
+            else {
+                flip (array, (array.length - numSort));
+                numSort += 1;
+            }
+        }
+    }
     return array;
 }
